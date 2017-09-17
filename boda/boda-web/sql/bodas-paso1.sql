27713bbdb3a5c05ec89ed97268afaafd5aa9d926
mf.valllejo <mf.valllejo@ISIS2603S4-0029.sis.virtual.uniandes.edu.co>
2017-09-17 18:07:32 -0500

    terminado asociaciones

--- boda/boda-web/sql/bodas-paso1.sql
+++ boda/boda-web/sql/bodas-paso1.sql
@@ -26,6 +26,12 @@ insert into calificacionentity (comentario, calificacionNum) values ('mGKZXTE PO
 
 --Pruebas para Invitado Entity
 delete from InvitadoEntity;
-insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (1, 'Miguel', 90570, 'mloughnan0@noaa.gov', false, 'trabajo');
-insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (2, 'Maddi', 82444, 'mpettengell1@yahoo.com', true, 'trabajo');
-insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (3, 'Griffy', 97900, 'gdeaguirre2@sina.com.cn', false, 'familia');
+insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (1, 'Miguel', 90570, 'mloughnan0@noaa.gov', 1, 'trabajo');
+insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (2, 'Maddi', 82444, 'mpettengell1@yahoo.com', 0, 'trabajo');
+insert into InvitadoEntity (id, name, documento, correo, asistencia, categoria) values (3, 'Griffy', 97900, 'gdeaguirre2@sina.com.cn', 0, 'familia');
+
+--Pruebas para Regalo Entity
+delete from RegaloEntity;
+insert into RegaloEntity (id, name, comprado, imagen) values (1, 'Cama', 1, 'https://robohash.org/addeseruntvero.jpg?size=50x50&set=set1');
+insert into RegaloEntity (id, name, comprado, imagen) values (2, 'Tendidos', 0, 'https://robohash.org/estutomnis.jpg?size=50x50&set=set1');
+insert into RegaloEntity (id, name, comprado, imagen) values (3, 'Lavadora', 0, 'https://robohash.org/eadoloremquearchitecto.bmp?size=50x50&set=set1');
