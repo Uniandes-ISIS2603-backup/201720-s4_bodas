77dfd033d7f0aee160e0eff7a100bdd9f7a3d86f
caguerrero <31454348+caguerrero@users.noreply.github.com>
2017-09-24 16:23:12 -0500

    Nuevo intento de merge ca.guerrero

---
+++ boda-web/src/main/java/co/edu/uniandes/csw/boda/dtos/TarjetaCreditoDetailDTO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.boda.dtos;
+
+import co.edu.uniandes.csw.boda.entities.TarjetaCreditoEntity;
+
+/**
+ *
+ * @author ca.guerrero
+ */
+public class TarjetaCreditoDetailDTO extends TarjetaCreditoDTO {
+    /**
+     * Constructor por defecto
+     */
+    public TarjetaCreditoDetailDTO() {
+    }
+    
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param entity
+     */
+    public TarjetaCreditoDetailDTO(TarjetaCreditoEntity entity) {
+        super(entity);
+    }
+    
+    /**
+     * Transformar un DTO a un Entity
+     *
+     * @return 
+     */
+    @Override
+    public TarjetaCreditoEntity toEntity() {
+        TarjetaCreditoEntity tarjetaE = super.toEntity();
+        return tarjetaE;
+    }
+}
