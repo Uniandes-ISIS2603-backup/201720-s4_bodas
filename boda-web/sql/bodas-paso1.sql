3a2b69c1a1d7f24c564ef5afb7dd1f5301f7d9f2
NIXON FERNANDO ORTIZ SUAREZ <nf.ortiz@sis.virtual.uniandes.edu.co>
2017-09-26 11:43:18 -0500

    Modificacion sql

--- boda-web/sql/bodas-paso1.sql
+++ boda-web/sql/bodas-paso1.sql
@@ -6,6 +6,7 @@ delete from InvitadoEntity;
 delete from calificacionentity;
 delete from OpcionServicioEntity;
 
+
 delete from TareaEntity;
 insert into ParejaEntity (correoElec, nombreInd1, nombreInd2, telefono, pago, contrasenia, nombreAbreviado, direccion) values ('fmarsland0@artisteer.com', 'Aaren', 'Ferrell', 86, 1, 'cGUDt68M', 'Agavaceae', '46 Gulseth Place');
 insert into ParejaEntity (correoElec, nombreInd1, nombreInd2, telefono, pago, contrasenia, nombreAbreviado, direccion) values ('bsimcoe1@dion.ne.jp', 'Elbertina', 'Baxy', 79, 0, '66tNlmFd', 'Asteraceae', '936 Mendota Plaza');
