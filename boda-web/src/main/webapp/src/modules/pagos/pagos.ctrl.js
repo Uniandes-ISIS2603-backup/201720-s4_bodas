65048c83f8877f94c10f44a7047d532d7d42a074
caguerrero <31454348+caguerrero@users.noreply.github.com>
2017-11-05 02:21:59 -0500

    Listando al fin los pagos y las tarjetas de credito como respectivos subrecursos

--- boda-web/src/main/webapp/src/modules/pagos/pagos.ctrl.js
+++ boda-web/src/main/webapp/src/modules/pagos/pagos.ctrl.js
@@ -1,45 +1,15 @@
 (function (ng) {
     var mod = ng.module("pagosModule");
-    mod.constant("pagosContext", "api/pagos");
-    mod.controller('pagosCtrl', ['$scope', '$http', 'pagosContext', '$state', '$stateParams',
-        function ($scope, $http, pagosContext, $state, $stateParams) {
-                $scope.records = {};
-                $http.get("data/pagos.json").then(function (response) {
-                $scope.records = response.data;
+    mod.constant("pagosContext", "pagos");
+    mod.constant("tarjetasCreditoContext", "tarjetasCredito");
+    mod.constant("parejasContext", "api/parejas");
+    mod.controller('pagosCtrl', ['$scope', '$http','parejasContext', 'tarjetasCreditoContext', '$state', 'pagosContext',
+        function ($scope, $http, parejasContext, tarjetasCreditoContext, $state, pagosContext) {
+                $http.get(parejasContext + '/' + $state.params.parejaId + '/' + tarjetasCreditoContext + '/' + $state.params.tarjetaId + '/' + pagosContext)
+                    .then(function (response) {
+                    $scope.pagosRecords = response.data;
             });
-            
-            if ($stateParams.pagosId !== undefined && $stateParams.pagosId !== null) {
-                id = $stateParams.pagosId;
-                $http.get(pagosContext + '/' + id).then(function (response) {
-                    $scope.currentPago = response.data;
-                });
-            } else {
-               
-                $scope.currentPago= {
-                    id: undefined,
-                    name: '',
-                };
-
-                $scope.alerts = [];
-            }
-            this.saveRecord = function (id) {
-                currentPago = $scope.currentPago;
-
-                // si el id es null, es un registro nuevo, entonces lo crea
-                if (id === null) {
-
-                    // ejecuta POST en el recurso REST
-                    return $http.post(pagosContext, currentPago)
-                            .then(function () {
-                                // $http.post es una promesa
-                                // cuando termine bien, cambie de estado
-                                $state.go('pagosList');
-                            });
-
-                    // si el id no es null, es un registro existente entonces lo actualiza
-                }
-            };
-        }
+    }
     ]);
 }
 )(angular);
