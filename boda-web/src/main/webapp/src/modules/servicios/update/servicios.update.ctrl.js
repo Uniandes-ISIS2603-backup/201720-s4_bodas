7a6547015bf11c8993b7b2aa6ecef4eb54e07675
ANDRES JAVIER  ORTIZ PEÐA <aj.ortiz10@sis.virtual.uniandes.edu.co>
2017-11-06 17:55:55 -0500

    adiciones front end servicio

--- boda-web/src/main/webapp/src/modules/servicios/update/servicios.update.ctrl.js
+++ boda-web/src/main/webapp/src/modules/servicios/update/servicios.update.ctrl.js
@@ -1,57 +1,35 @@
 (
         function (ng) {
-            var mod = ng.module("bodasModule");
-            mod.constant("bodasContext", "api/bodas");
+            var mod = ng.module("serviciosModule");
+            mod.constant("serviciosContext", "api/servicios");
            // mod.constant("regalosContext", "api/regalos");
-            mod.controller('bodaUpdateCtrl', ['$scope', '$http', 'bodasContext', '$state', '$rootScope', '$filter',
-                function ($scope, $http, bodasContext, $state, $rootScope ) {
+            mod.controller('servicioUpdateCtrl', ['$scope', '$http', 'serviciosContext', '$state', '$rootScope', '$filter',
+                function ($scope, $http, serviciosContext, $state, $rootScope ) {
                     $rootScope.edit = true;
 
-                    var idBoda = $state.params.bodaId;
+                    var idServicio = $state.params.servicioId;
 
-                    // Este arreglo guardara los ids de los regalos asociados y por asociar al autor.
-                    //var idsRegalo = [];
 
-                    // Este arreglo mostrará los regalos una vez esten filtrados visualmente por lo que el autor ya tiene asociado.
-                    //$scope.allRegalosShow = [];
 
-                    //Consulto la boda a editar.
-                    $http.get(bodasContext + '/' + idBoda).then(function (response) {
-                        var boda = response.data;
-                        $scope.bodaName = boda.name;
-                        $scope.bodaFecha = new Date(boda.fecha);
-                        $scope.bodaReligion = boda.description;
-                        $scope.bodaTema = boda.description;
-                        $scope.bodaTipo = boda.description;
-                        $scope.bodaImage = boda.image;
+                    //Consulto la servicio a editar.
+                    $http.get(serviciosContext + '/' + idServicio).then(function (response) {
+                        var servicio = response.data;
+                        $scope.servicioName = servicio.name;
+                        $scope.servicioDescripcion = servicio.description;
                         
-                        //$scope.allRegalosBoda = boda.regalos;
-                        //$scope.mergeRegaloss($scope.allRegalosBoda);
                     });
                     
 
                   //Faltan los metodos de regalos e invitados
 
-                    $scope.createBoda = function () {
-                        /*Se llama a la función newRegalos() para buscar cada uno de los ids de los regalos
-                         en el array que tiene todos los regalos y así saber como queda la lista final de los regalos asociados a la boda.
-                         */
-                        //$scope.newRegalos();
-                        $http.put(bodasContext + "/" + idBoda, {
-                            fecha: $scope.bodaFecha,
-                            image: $scope.bodaImage,
-                            name: $scope.bodaName,
-                            religion: $scope.bodaReligion,
-                            tema: $scope.bodaTema,
-                            tipo: $scope.bodaTipo
-                        //}).then(function (response) {
-                        //    if (idsRegalos.length >= 0) {
-                         //       $http.put(bodasContext + "/" + response.data.id + "/regalos", $scope.allRegalosBoda).then(function (response) {
-                           //     });
-                            //}
-                            //Boda created successfully
+                    $scope.createServicio = function () {
+   
+                        //$scope.newServicios();
+                        $http.put(serviciosContext + "/" + idServicio, {
+                            name: $scope.servicioName,
+                            descripcion: $scope.servicioDescripcion
                             }).then(function (response) {
-                            $state.go('bodasList', {bodaId: response.data.id}, {reload: true});
+                            $state.go('serviciosList', {servicioId: response.data.id}, {reload: true});
                             
                         });
                     };
