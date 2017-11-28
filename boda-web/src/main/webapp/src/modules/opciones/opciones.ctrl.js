64c626b4e840e6991c4a56e5a26b19db5db00bcb
caguerrero <31454348+caguerrero@users.noreply.github.com>
2017-11-28 16:08:07 -0500

    Arreglos al contratar una opcionServicio

--- boda-web/src/main/webapp/src/modules/opciones/opciones.ctrl.js
+++ boda-web/src/main/webapp/src/modules/opciones/opciones.ctrl.js
@@ -3,17 +3,18 @@
     var mod = ng.module("opcionesModule");
     mod.constant("opcionesServicioContext", "opcionServicios");
     mod.constant("proveedoresContext", "api/proveedores");
-    mod.controller('opcionesCtrl', ['$scope', '$http', 'proveedoresContext', '$state', 'opcionesContext',
+    mod.controller('opcionesCtrl', ['$scope', '$http', 'proveedoresContext', '$state', 'opcionesContext','$rootScope',
         
-        function ($scope, $http, proveedoresContext, $state, opcionesContext) {
+        function ($scope, $http, proveedoresContext, $state, opcionesContext,$rootScope) {
           
             $http.get(proveedoresContext + '/' + $state.params.proveedorId + '/' + opcionesContext).then(function (response) {
                 $scope.opcionesRecords = response.data;
             });
        if ($state.params.opcionId !== undefined) {
                 $http.get(proveedoresContext + '/' + $state.params.proveedorId + '/' +opcionesContext + '/' + $state.params.opcionId).then(function (response) {
-                   
+                  
                     $scope.currentOpcion = response.data;
+                     $rootScope.currentOpcionPago=response.data;
                 });   
             }
     
